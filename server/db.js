420f090f78073ccf16a6575e4f0805c1cd3f9863
Coleb <coleb@gmail.com>
2026-05-14 09:46:01 +0300

    chore: update db.js to support DB_PATH env var for Railway deployment

--- server/db.js
+++ server/db.js
@@ -1,7 +1,7 @@
 const Database = require('better-sqlite3');
 const path = require('path');
 
-const DB_PATH = path.join(__dirname, 'watermonitor.db');
+const DB_PATH = process.env.DB_PATH || path.join(__dirname, 'watermonitor.db');
 let db;
 const add = sql => { try { db.exec(sql); } catch {} };
 
